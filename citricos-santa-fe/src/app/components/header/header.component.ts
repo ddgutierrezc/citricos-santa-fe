cc10cfbd0b1fc6f6e53d6c2ef9e4801c193236eb
David Gutiérrez <101062029+ScreimerTV@users.noreply.github.com>
2023-06-26 17:51:51 -0600

    agregue el navbar
    agregue el navbar

--- citricos-santa-fe/src/app/components/header/header.component.ts
+++ citricos-santa-fe/src/app/components/header/header.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DividerComponent } from '../divider/divider.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DividerComponent],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
--- citricos-santa-fe/src/app/pages/home/home.component.ts
+++ citricos-santa-fe/src/app/pages/home/home.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from 'src/app/components/header/header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
