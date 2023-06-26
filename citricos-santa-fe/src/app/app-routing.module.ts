01a989e95667c9b47e54d2ee55e07d6e43a1f91f
David Gutiérrez <101062029+ScreimerTV@users.noreply.github.com>
2023-06-26 16:21:32 -0600

    version inicial terminada
    version inicial terminada

--- citricos-santa-fe/src/app/app-routing.module.ts
+++ citricos-santa-fe/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { URLPagesEnum } from './common/enums/url-pages.enum';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: URLPagesEnum.HOME,
+    pathMatch: 'full',
+  },
+  {
+    path: URLPagesEnum.HOME,
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ citricos-santa-fe/src/app/common/enums/url-pages.enum.ts
@@ -0,0 +1,3 @@
+export enum URLPagesEnum {
+  HOME = 'home',
+}
