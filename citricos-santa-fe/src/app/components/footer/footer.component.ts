1f911a9439218534b3822f649de41f2291c1457f
David Gutiérrez <101062029+ScreimerTV@users.noreply.github.com>
2023-06-27 21:15:38 -0600

    update links redes sociales footer
    update links redes sociales footer

--- citricos-santa-fe/src/app/components/footer/footer.component.ts
+++ citricos-santa-fe/src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LinksRedesSocialesEnum } from 'src/app/common/enums/links-redes-sociales.enum';
 import { TextosAlternativosEnum } from 'src/app/common/enums/textos-alternativos.enum';
 
 @Component({
@@ -18,5 +19,16 @@ export class FooterComponent {
    */
   get textosAlternativosEnum(): typeof TextosAlternativosEnum {
     return TextosAlternativosEnum;
+  }
+
+  /**
+   * Obtiene el enumerado con los links de las redes sociales.
+   * @returns {LinksRedesSocialesEnum} Enumerado con los links de las redes sociales.
+   * @author dgutierrez
+   * @version 1.0
+   * @since 27/06/2023
+   */
+  get redesSocialesEnum(): typeof LinksRedesSocialesEnum {
+    return LinksRedesSocialesEnum;
   }
 }
