c5d7d50c2740cdcc8fc2eabff27c260126b474e5
David Daniel Gutiérrez Calderón <148672101+ddgutierrezc@users.noreply.github.com>
2024-09-16 14:31:09 -0600

    se pasa a zonelss

--- citricos-santa-fe/src/app/app.module.ts
+++ citricos-santa-fe/src/app/app.module.ts
@@ -1,4 +1,7 @@
-import { NgModule } from '@angular/core';
+import {
+  NgModule,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,7 +11,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
-  providers: [],
+  providers: [provideExperimentalZonelessChangeDetection()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
